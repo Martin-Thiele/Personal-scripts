852d330013132dcc5415407530dc22358d129b7a
Martin Thiele <Martin Thiele>
2017-03-10 17:20:22 +0100

    more functionality

--- Windows/Mp3download/mdl.bat
+++ Windows/Mp3download/mdl.bat
@@ -1,4 +1,8 @@
 @echo off
-set lul=%1 %2 %3 %4 %5 %6 %7 %8
-shift
-youtube-dl -f bestaudio --ffmpeg-location C:\Users\minic\Documents\Github\Personal-scripts\Windows\Mp3download --extract-audio --audio-format mp3 "ytsearch:%lul%" -o "F:\Dokumenter\Musik\Youtube\%%(title)s.^%%(ext)s"
+if %1%==--dl (
+	explorer "F:\Dokumenter\Musik\Youtube\"
+) else (
+	set lul=%1 %2 %3 %4 %5 %6 %7 %8
+	shift
+	youtube-dl -f bestaudio --ffmpeg-location C:\Users\minic\Documents\Github\Personal-scripts\Windows\Mp3download --extract-audio --audio-format mp3 "ytsearch:%lul%" -o "F:\Dokumenter\Musik\Youtube\%%(title)s.^%%(ext)s"
+)
